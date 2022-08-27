ea8fcaef846de7c971fd5ae0091512815796a46a
shellsider <work.harshdeepsingh@gmail.com>
2022-08-27 22:39:42 +0530

    added data.js and data.js

---
+++ data.js
@@ -0,0 +1,304 @@
+const data = [
+    {
+      "_id": "630a4e06b6e223ad6401713a",
+      "index": 0,
+      "age": 87,
+      "name": "Hurst Moon"
+    },
+    {
+      "_id": "630a4e0624387f493ef3b040",
+      "index": 1,
+      "age": 1,
+      "name": "Gregory Rojas"
+    },
+    {
+      "_id": "630a4e065993ba89a0632d2e",
+      "index": 2,
+      "age": 45,
+      "name": "Mai Bauer"
+    },
+    {
+      "_id": "630a4e06f6ff7e73e387f6b4",
+      "index": 3,
+      "age": 86,
+      "name": "Wagner Stafford"
+    },
+    {
+      "_id": "630a4e067805a5859c06dbd7",
+      "index": 4,
+      "age": 90,
+      "name": "Jayne Ellis"
+    },
+    {
+      "_id": "630a4e063904f657b6bce16b",
+      "index": 5,
+      "age": 20,
+      "name": "Esther Mann"
+    },
+    {
+      "_id": "630a4e06816f56dd8797ee61",
+      "index": 6,
+      "age": 56,
+      "name": "Melanie Kaufman"
+    },
+    {
+      "_id": "630a4e06f95822a2d86c7af8",
+      "index": 7,
+      "age": 31,
+      "name": "Woods Gordon"
+    },
+    {
+      "_id": "630a4e065eeacfc24747fdf4",
+      "index": 8,
+      "age": 61,
+      "name": "Virginia Cabrera"
+    },
+    {
+      "_id": "630a4e061946c41c84c9bad0",
+      "index": 9,
+      "age": 83,
+      "name": "Connie Wright"
+    },
+    {
+      "_id": "630a4e065490241b2ef0cea7",
+      "index": 10,
+      "age": 35,
+      "name": "Mattie Herring"
+    },
+    {
+      "_id": "630a4e061c33d9abdfc7f925",
+      "index": 11,
+      "age": 25,
+      "name": "Mcmillan Snyder"
+    },
+    {
+      "_id": "630a4e06291d02c37d5a8ff5",
+      "index": 12,
+      "age": 88,
+      "name": "Freeman Mcclain"
+    },
+    {
+      "_id": "630a4e064e3a35cc3894141c",
+      "index": 13,
+      "age": 40,
+      "name": "Willa Marks"
+    },
+    {
+      "_id": "630a4e06481f484afb70b569",
+      "index": 14,
+      "age": 29,
+      "name": "House Robinson"
+    },
+    {
+      "_id": "630a4e06b7b995c5d973d9dc",
+      "index": 15,
+      "age": 20,
+      "name": "Mathews Albert"
+    },
+    {
+      "_id": "630a4e0612c58bb1dad22397",
+      "index": 16,
+      "age": 47,
+      "name": "Kimberley Steele"
+    },
+    {
+      "_id": "630a4e064cdec4c3f697711f",
+      "index": 17,
+      "age": 84,
+      "name": "Johnson Flores"
+    },
+    {
+      "_id": "630a4e06a508bbcaa7fd3fbb",
+      "index": 18,
+      "age": 87,
+      "name": "Rowe Fulton"
+    },
+    {
+      "_id": "630a4e0628ad5dcca0bbc65e",
+      "index": 19,
+      "age": 19,
+      "name": "Wilma Lindsay"
+    },
+    {
+      "_id": "630a4e06036134c08c02d232",
+      "index": 20,
+      "age": 45,
+      "name": "Bernadette Dixon"
+    },
+    {
+      "_id": "630a4e06f91bf6dc91c6c49e",
+      "index": 21,
+      "age": 33,
+      "name": "Janette Jones"
+    },
+    {
+      "_id": "630a4e066439cdb79900cf02",
+      "index": 22,
+      "age": 59,
+      "name": "Estella Levine"
+    },
+    {
+      "_id": "630a4e0696d42e4876be3701",
+      "index": 23,
+      "age": 31,
+      "name": "Rosalinda Solomon"
+    },
+    {
+      "_id": "630a4e0615e79c3c93322ff1",
+      "index": 24,
+      "age": 80,
+      "name": "Lopez Noel"
+    },
+    {
+      "_id": "630a4e063257aeaf248ddd3e",
+      "index": 25,
+      "age": 11,
+      "name": "Schwartz Whitley"
+    },
+    {
+      "_id": "630a4e06a5d14265583dfdc6",
+      "index": 26,
+      "age": 41,
+      "name": "Taylor Brady"
+    },
+    {
+      "_id": "630a4e06eb9eeeeb47d4a099",
+      "index": 27,
+      "age": 23,
+      "name": "Neal Bean"
+    },
+    {
+      "_id": "630a4e063a57ebbbadb0ec7e",
+      "index": 28,
+      "age": 81,
+      "name": "Mccall Figueroa"
+    },
+    {
+      "_id": "630a4e06ff553bdeb721c3b9",
+      "index": 29,
+      "age": 11,
+      "name": "Moreno Stephenson"
+    },
+    {
+      "_id": "630a4e064c9f0a33ebb74746",
+      "index": 30,
+      "age": 77,
+      "name": "Wilson Cantu"
+    },
+    {
+      "_id": "630a4e06d8b15f45009b26b5",
+      "index": 31,
+      "age": 59,
+      "name": "Courtney Ryan"
+    },
+    {
+      "_id": "630a4e063f64f30435db2ca2",
+      "index": 32,
+      "age": 57,
+      "name": "Trina Patterson"
+    },
+    {
+      "_id": "630a4e064605eb7e342763cb",
+      "index": 33,
+      "age": 49,
+      "name": "White Weeks"
+    },
+    {
+      "_id": "630a4e06da9c0d41a7addb6e",
+      "index": 34,
+      "age": 23,
+      "name": "Luisa Barry"
+    },
+    {
+      "_id": "630a4e066d44d06fe10e2b6a",
+      "index": 35,
+      "age": 20,
+      "name": "Clarice Hickman"
+    },
+    {
+      "_id": "630a4e06fc22233e09435e7a",
+      "index": 36,
+      "age": 71,
+      "name": "Hays Cooley"
+    },
+    {
+      "_id": "630a4e06af633f54626969aa",
+      "index": 37,
+      "age": 71,
+      "name": "Pittman Castro"
+    },
+    {
+      "_id": "630a4e0647df41ee051ca7f9",
+      "index": 38,
+      "age": 34,
+      "name": "Guadalupe Juarez"
+    },
+    {
+      "_id": "630a4e0640d6ecfa1e0dfc62",
+      "index": 39,
+      "age": 44,
+      "name": "Callahan Hopkins"
+    },
+    {
+      "_id": "630a4e06fbed01156bb3287a",
+      "index": 40,
+      "age": 78,
+      "name": "Valentine Boyd"
+    },
+    {
+      "_id": "630a4e064407e1de29e8ef96",
+      "index": 41,
+      "age": 87,
+      "name": "Abby Barton"
+    },
+    {
+      "_id": "630a4e0653c6b2a0198b2f26",
+      "index": 42,
+      "age": 9,
+      "name": "Rios Bridges"
+    },
+    {
+      "_id": "630a4e06b039764bb4177f2b",
+      "index": 43,
+      "age": 32,
+      "name": "Alvarez Avery"
+    },
+    {
+      "_id": "630a4e0655427961998a8071",
+      "index": 44,
+      "age": 12,
+      "name": "Murphy Vaughan"
+    },
+    {
+      "_id": "630a4e06ceb3daed9be75f98",
+      "index": 45,
+      "age": 53,
+      "name": "Aurelia Sloan"
+    },
+    {
+      "_id": "630a4e0662265ce6733c503f",
+      "index": 46,
+      "age": 30,
+      "name": "Floyd Brooks"
+    },
+    {
+      "_id": "630a4e06c849f4447653d1d4",
+      "index": 47,
+      "age": 54,
+      "name": "Frankie Morin"
+    },
+    {
+      "_id": "630a4e06207dbff16904dbaa",
+      "index": 48,
+      "age": 7,
+      "name": "Guerrero Alvarez"
+    },
+    {
+      "_id": "630a4e06668f07ef6298981a",
+      "index": 49,
+      "age": 78,
+      "name": "Dorothy Carpenter"
+    }
+  ]
+
+module.exports = data
