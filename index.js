0b1e8e7b1844e4b921d30c3ac1764347bd3babd5
shellsider <work.harshdeepsingh@gmail.com>
2022-08-27 22:54:46 +0530

    updated index.js
    Wasup world my name is kakapreet singh :)

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const data = require('./data')
-console.log(data.length)
+let totalUser = data.length;
+let underAgeUsers  =0;
+let eligibleUsers = 0;
+
+data.forEach((user)=>{
+    if(user.age>18) eligibleUsers += 1
+    else underAgeUsers +=1
+})
+
+console.log({
+    Total_Users:totalUser,
+    Eligible_Users:eligibleUsers,
+    Under_Age_Users:underAgeUsers
+})
